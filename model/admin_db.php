9c7fa10a6e0985e74702f6528d9409e5955596d2
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-03-31 19:42:42 -0500

    Try [0] !== 0

--- model/admin_db.php
+++ model/admin_db.php
@@ -37,7 +37,7 @@
         $row = $statement->fetch();
         $statement->closeCursor();
         if(isset($row)) {
-            return $row[0] !== '0';
+            return $row[0] !== 0;
         }
     }
 ?>
