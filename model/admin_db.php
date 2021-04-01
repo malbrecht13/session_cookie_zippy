f1aad209dd4ad58596be621e1b0e7be27ddeb67d
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-03-31 19:51:11 -0500

    Forget about empty()

--- model/admin_db.php
+++ model/admin_db.php
@@ -37,7 +37,7 @@
         $row = $statement->fetch();
         $statement->closeCursor();
         if(isset($row)) {
-            return true;
+            return $row[0] > 0;
         } else {
             return false;
         }
