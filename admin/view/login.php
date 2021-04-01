a5ea5796837287f813aa48b69d518e4ee1ac39de
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-03-31 23:53:10 -0500

    Added styling to login.php

--- admin/view/login.php
+++ admin/view/login.php
@@ -2,6 +2,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">    
    
     <link rel="stylesheet" href="../View/css/style.css">
+    <style>
+        
+        h1 {
+            font-size: 1rem;
+        }
+
+        h2 {
+            font-size: 0.9rem;
+        }
+        h3 {
+            font-size: 0.8rem;
+        }
+
+        .add_vehicle__input {
+            display: flex;
+            flex-direction: column;
+        }
+        .add_vehicle__button {
+            margin-top: 10px;
+        }
+        select, input[type="number"], input[type="text"] {
+            border-radius: 5px;
+            padding: 3px;
+            margin-bottom: 10px;
+        }
+        .edit_vehicle__form {
+            border: 3px solid black;
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+            margin: 0 auto;
+            padding: 30px;
+            max-width: 300px;
+            min-width: 200px;
+        }
+    </style>
 </head>
 <body class="container-fluid text-center">
     <main>
