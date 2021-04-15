e46e0a23e25092b374f62d3d83373f93a67655dc
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-04-14 23:59:31 -0500

    Added  as private variable

--- model/database.php
+++ model/database.php
@@ -3,6 +3,7 @@ class Database {
     private static $dsn = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=gvdsr24jhvu8j757';
     private static $username = 'dzq33wn2lmpzso31';
     private static $password = 'l3pzfll323cekhd8';
+    private static $db;
 
     private function __construct() {}
 
